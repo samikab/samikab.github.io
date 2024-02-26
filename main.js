4b0013cfd0d7dc7672a20b8f698a1bbb29a4323b
samikab <157431911+samikab@users.noreply.github.com>
2024-02-26 18:53:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function myFunction() {
     var name = prompt("Enter Name!");
     var greeting = "HI " + name + "! WELCOME TO MY WEBSITE!!";
-    document.getElementById("greeting").innerHTML=greeting;
-}
+    document.getElementById("greeting").innerHTML=greeting;}
 
+function ChangeImageSize(){
+    var img = document.getElementById("Pic");
+    img.style.height = "1000px";
+    img.style.width = "700px"; }
