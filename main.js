e4daef41547e074a63e4335f7bbf2dd5794df543
samikab <157431911+samikab@users.noreply.github.com>
2024-02-26 16:23:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function myFunction() {
     var name = prompt("Click me!");
     var greeting = "Hi " + name + "! Thank you for visiting my website :)";
-    document.getElementById("greeting").innerHTML = greeting;
 }
 
