55cb14505f4bdd2068d2c486803c3273f1bcb5ac
samikab <157431911+samikab@users.noreply.github.com>
2024-02-26 19:29:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+
+//Greet Viwers 
 function myFunction() {
     var name = prompt("Enter Name!");
     var greeting = "HI " + name + "! WELCOME TO MY WEBSITE!!";
     document.getElementById("greeting").innerHTML=greeting;}
 
+// Increase image size once clicked 
 function ChangeImageSize(){
     var img = document.getElementById("Pic");
     img.style.height = "1000px";
     img.style.width = "700px"; }
+
+function showDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'block';
+}
+
+function hideDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'none';}
